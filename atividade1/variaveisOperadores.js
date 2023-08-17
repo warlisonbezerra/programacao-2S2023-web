3d6267f44fa5c179303e58002abc0c208766a7a0
WARLISON DA SILVA BEZERRA <warlison.bezerra@a.ucb.br>
2023-08-17 10:19:51 -0300

    teste

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+// variáveis e seus valores
+var a = 5;
+var b = 10;
+
+// cálculos a serem feitos com as variáveis
+var soma = a + b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+console.log(`a soma dos valores ${a} + ${b} = ${soma}`);
+console.log(`a subtração dos valores ${a} - ${b} = ${sub}`);
+console.log(`a multiplicação dos valores ${a} * ${b} = ${mult}`);
+console.log(`a divisão dos valores ${a} / ${b} = ${div}`);
+
