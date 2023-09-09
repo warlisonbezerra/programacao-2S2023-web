83953951678d5db8270e5eb8173016f3c35427e6
warlisonbezerra <warlisonbezerra1@gmail.com>
2023-09-09 06:58:06 -0300

    código do professor

--- atividade4/index.js
+++ atividade4/index.js
@@ -1,37 +1,16 @@
+const calc = require('./util/calculadora');
 const express = require('express');
+
 const app = express();
-const calculadora = require('./util/calculadora');
 
-// Rota para somar
-app.get('/somar/:a/:b', (req, res) => {
-  const { a, b } = req.params;
-  const resultado = calculadora.somar(Number(a), Number(b));
-  res.send(`Resultado: ${resultado}`);
+
+app.get("/somar/:a/:b", function (req, res) {
+    let a = req.params.a;
+    let b = req.params.b;
+    res.send(`${a} + ${b} = ${calc.somar(a, b)}`);
 });
 
-// Rota para subtrair
-app.get('/subtrair/:a/:b', (req, res) => {
-  const { a, b } = req.params;
-  const resultado = calculadora.subtrair(Number(a), Number(b));
-  res.send(`Resultado: ${resultado}`);
-});
-
-// Rota para multiplicar
-app.get('/multiplicar/:a/:b', (req, res) => {
-  const { a, b } = req.params;
-  const resultado = calculadora.multiplicar(Number(a), Number(b));
-  res.send(`Resultado: ${resultado}`);
-});
-
-// Rota para dividir
-app.get('/dividir/:a/:b', (req, res) => {
-  const { a, b } = req.params;
-  const resultado = calculadora.dividir(Number(a), Number(b));
-  res.send(`Resultado: ${resultado}`);
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log("app rodando na porta "+PORT);
 });
